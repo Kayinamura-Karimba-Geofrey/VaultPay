2afab8955b82c493e1b6b20e0df5bd7db5c02dd7
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-01-16 09:31:27 +0200

    user

--- VaultPay/src/main/java/com/example/VaultPay/User/User.java
+++ VaultPay/src/main/java/com/example/VaultPay/User/User.java
@@ -1,22 +1,47 @@
-package com.example.VaultPay.user;
+package com.example.vaultpay.user;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable=false, unique=true)
+    @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    private boolean isEnabled = true;
-    private boolean isLocked = false;
+    private boolean enabled = true;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override public String getPassword() { return password; }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return enabled; }
 }
