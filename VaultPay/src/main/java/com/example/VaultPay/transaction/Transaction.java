ac3b9674f92499dfc53d5b5d4c3cad612a6750ba
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-01-17 21:43:18 +0200

    user

---
+++ VaultPay/src/main/java/com/example/VaultPay/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.vaultpay.transaction;
+
+import com.vaultpay.wallet.Wallet;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Wallet wallet;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private String reference;
+}
