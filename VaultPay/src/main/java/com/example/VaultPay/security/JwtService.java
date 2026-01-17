ed779f9bb7e5e84993ffbe7a87bc738de7a6202d
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-01-17 20:53:08 +0200

    user

--- VaultPay/src/main/java/com/example/VaultPay/security/JwtService.java
+++ VaultPay/src/main/java/com/example/VaultPay/security/JwtService.java
@@ -1,25 +1,39 @@
-package com.example.VaultPay.security;
+package com.vaultpay.security;
 
 import io.jsonwebtoken.*;
 import org.springframework.stereotype.Service;
 
-@service
-public class JwtService{
-    private final string SECRET= "VaultPay_SECURE_SECRET_KEY";
-    private final long EXPIRATION= 1000*60*15;
-    public String generatedToken(string email){
-        return Jwts.buider()
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET = "VAULTPAY_SUPER_SECURE_SECRET_KEY";
+    private final long ACCESS_EXP = 1000 * 60 * 15;       // 15 min
+    private final long REFRESH_EXP = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+    public String generateAccessToken(String email) {
+        return buildToken(email, ACCESS_EXP);
+    }
+
+    public String generateRefreshToken(String email) {
+        return buildToken(email, REFRESH_EXP);
+    }
+
+    private String buildToken(String email, long expiration) {
+        return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION))
-                .signWith(SignatureAlgorithm.HS256,SECRET)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
                 .compact();
     }
-    public String extractEmail(String token){
-        return Jwts.parser().setSigningKey(SECRET)
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
     }
 }
-
