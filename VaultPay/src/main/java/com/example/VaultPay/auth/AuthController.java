fa1f8d4fe4fa39664549587de01a4a3ab0b25280
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-01-17 21:17:01 +0200

    user

--- VaultPay/src/main/java/com/example/VaultPay/auth/AuthController.java
+++ VaultPay/src/main/java/com/example/VaultPay/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.vaultpay.auth;
 
 import com.vaultpay.auth.dto.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +12,19 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/register")
-    public AuthResponse register(@RequestBody RegisterRequest request) {
-        return authService.register(request);
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+
+    @PostMapping("/refresh")
+    public AuthResponse refresh(@RequestBody RefreshRequest request) {
+        return authService.refresh(request);
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request) {
+        String token = request.getHeader("Authorization").substring(7);
+        authService.logout(token);
     }
 }
