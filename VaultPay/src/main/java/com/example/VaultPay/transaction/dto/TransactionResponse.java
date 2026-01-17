a4f24cb91d0a3a060172155c6673f0dd33a759f0
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-01-17 22:00:38 +0200

    user

--- VaultPay/src/main/java/com/example/VaultPay/transaction/dto/TransactionResponse.java
+++ VaultPay/src/main/java/com/example/VaultPay/transaction/dto/TransactionResponse.java
@@ -1,11 +1,18 @@
 package com.vaultpay.transaction.dto;
 
+import com.vaultpay.transaction.TransactionType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
-public class TransferResponse {
-    private String status;
+public class TransactionResponse {
+
+    private TransactionType type;
+    private BigDecimal amount;
     private String reference;
+    private LocalDateTime createdAt;
 }
