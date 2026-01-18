bff013d425e4d44b21b71edcac0185fb41c23815
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-01-18 18:47:31 +0200

    user

--- VaultPay/src/main/java/com/example/VaultPay/auth/AuthService.java
+++ VaultPay/src/main/java/com/example/VaultPay/auth/AuthService.java
@@ -1,7 +1,13 @@
-package com.example.VaultPay.auth;
+package com.vaultpay.auth;
 
-import com.example.VaultPay.auth.dto.*;
-import com.example.VaultPay.security.*;
+import com.example.VaultPay.auth.dto.AuthResponse;
+import com.example.VaultPay.auth.dto.LoginRequest;aQ
+import com.example.VaultPay.audit.AuditAction;
+import com.example.VaultPay.audit.AuditService;
+import com.example.VaultPay.security.JwtService;
+import com.example.VaultPay.security.TokenBlacklist;
+import com.example.VaultPay.security.TokenBlacklistRepository;
+import com.example.VaultPay.user.User;
 import com.example.VaultPay.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,15 +23,24 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final TokenBlacklistRepository blacklistRepository;
+    private final AuditService auditService;
+
 
     public AuthResponse login(LoginRequest request) {
 
-        var user = userRepository.findByEmail(request.getEmail())
+        User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new RuntimeException("Invalid credentials"));
 
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new RuntimeException("Invalid credentials");
         }
+
+
+        auditService.log(
+                AuditAction.USER_LOGGED_IN,
+                user,
+                "User logged in successfully"
+        );
 
         return new AuthResponse(
                 jwtService.generateAccessToken(user.getEmail()),
@@ -33,22 +48,26 @@ public class AuthService {
         );
     }
 
-    public AuthResponse refresh(RefreshRequest request) {
-
-        String email = jwtService.extractEmail(request.getRefreshToken());
-
-        return new AuthResponse(
-                jwtService.generateAccessToken(email),
-                request.getRefreshToken()
-        );
-    }
 
     public void logout(String token) {
+
+        String email = jwtService.extractEmail(token);
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
         blacklistRepository.save(
                 TokenBlacklist.builder()
                         .token(token)
                         .invalidatedAt(LocalDateTime.now())
                         .build()
+        );
+
+
+        auditService.log(
+                AuditAction.USER_LOGGED_OUT,
+                user,
+                "User logged out"
         );
     }
 }
