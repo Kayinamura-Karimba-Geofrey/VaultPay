f3e649b0f0fb37b0682bc117852a3f5164c4223f
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-03-01 10:47:35 +0200

    products

---
+++ VaultPay/src/main/java/com/Trial/security/TokenBlacklistRspository.java
@@ -0,0 +1,9 @@
+package com.vaultpay.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenBlacklistRepository
+        extends JpaRepository<TokenBlacklist, Long> {
+
+    boolean existsByToken(String token);
+}
