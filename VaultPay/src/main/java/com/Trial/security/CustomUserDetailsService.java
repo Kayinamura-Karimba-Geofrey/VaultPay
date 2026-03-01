314377babb4f1388bd2a890fe6e57101d895f990
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-03-01 10:31:14 +0200

    products

--- VaultPay/src/main/java/com/Trial/security/CustomUserDetailsService.java
+++ VaultPay/src/main/java/com/Trial/security/CustomUserDetailsService.java
@@ -1,22 +1,24 @@
-package Com.VaultPay.security;
+package com.vaultpay.security;
 
-import com.VaultPay.user.UserRepository;
+import com.vaultpay.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import  org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotfoundException;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService{
+public class CustomUserDetailsService implements UserDetailsService {
+
     private final UserRepository userRepository;
 
-    Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-        return userRepository.findByUsername(username) throws usernameNotFoundException{
-            return userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("User not found "));
-            
-        }
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new SecurityUser(user);
     }
 }
