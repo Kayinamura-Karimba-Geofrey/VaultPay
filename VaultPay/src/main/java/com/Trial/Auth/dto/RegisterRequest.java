31bc496f98420c790ec1095982ca69f47a625c07
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-03-02 00:03:11 +0200

    products

---
+++ VaultPay/src/main/java/com/Trial/Auth/dto/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.vaultpay.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    @NotBlank
+    private String fullName;
+}
