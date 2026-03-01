11517d513d92a52863f6a1d36a9acdf56957e457
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-03-02 01:31:42 +0200

    products

--- VaultPay/src/main/java/com/Trial/config/JacksonConfig.java
+++ VaultPay/src/main/java/com/Trial/config/JacksonConfig.java
@@ -9,21 +9,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JacksonConfig {
 
-    /**
-     * Custom ObjectMapper for:
-     * - LocalDateTime support
-     * - ISO date formatting
-     * - Stable JSON responses
-     */
+
     @Bean
     public ObjectMapper objectMapper() {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        // Support Java 8+ date/time types
+
         mapper.registerModule(new JavaTimeModule());
 
-        // Use ISO date format instead of timestamps
+
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
         return mapper;
