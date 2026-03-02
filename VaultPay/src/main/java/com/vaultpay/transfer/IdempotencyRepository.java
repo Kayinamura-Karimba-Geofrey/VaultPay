1b4ecdb5145a6389543756bd962965d4c5745175
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-03-02 12:12:06 +0200

    products

---
+++ VaultPay/src/main/java/com/vaultpay/transfer/IdempotencyRepository.java
@@ -0,0 +1,10 @@
+package com.vaultpay.transfer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IdempotencyRepository extends JpaRepository<IdempotencyKey, Long> {
+
+    Optional<IdempotencyKey> findByIdempotencyKey(String idempotencyKey);
+}
